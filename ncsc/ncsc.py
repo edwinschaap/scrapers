07738c367c9c998ae5518f402347300bad10ee0b
Edwin Schaap <edwin@schaap.me>
2017-12-07 20:56:11 +0100

    soup the response

--- ncsc/ncsc.py
+++ ncsc/ncsc.py
@@ -2,12 +2,18 @@
 # Scrape advisories from ncsc.nl
 
 import requests
+from bs4 import BeautifulSoup
+
+def getSoupFromUrl(url):
+    response = requests.get(url)
+    html = response.content
+    return BeautifulSoup(html, "html.parser")
 
 def main():
     url = "https://www.ncsc.nl/dienstverlening/response-op-dreigingen-en-incidenten/beveiligingsadviezen/NCSC-2017-1058+1.00+Kwetsbaarheden+verholpen+in+Apple+iOS.html"
-    response=requests.get(url)
+    soup = getSoupFromUrl(url)
 
-    print(response)
+    print(soup)
 
 if __name__ == '__main__':
     main()
