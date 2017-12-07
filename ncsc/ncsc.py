ad765bd80718f63d62d2b2cca498680cf608493e
Edwin Schaap <edwin@schaap.me>
2017-12-07 21:28:30 +0100

    lets verify the message content

--- ncsc/ncsc.py
+++ ncsc/ncsc.py
@@ -3,6 +3,9 @@
 
 import requests
 from bs4 import BeautifulSoup
+import gnupg
+
+gpg = None
 
 def getSoupFromUrl(url):
     response = requests.get(url)
@@ -13,10 +16,14 @@ def parseAdvisoryPage(url):
     response = getSoupFromUrl(url)
     advisory = {}
     advisory['title'] = response.select('.advisoryitem h3')[0].contents[0].strip()
+    advisory['message'] = response.select('.advisoryitem pre')[0].contents[0]
+    advisory['verified'] = gpg.verify(advisory['message'])
 
     return advisory
 
 def main():
+    global gpg
+    gpg = gnupg.GPG()
     url = "https://www.ncsc.nl/dienstverlening/response-op-dreigingen-en-incidenten/beveiligingsadviezen/NCSC-2017-1058+1.00+Kwetsbaarheden+verholpen+in+Apple+iOS.html"
     advisory = parseAdvisoryPage(url)
 
