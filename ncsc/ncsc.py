a9468602f7c10a0f95c799ed01b7fe67545b9b11
Edwin Schaap <edwin@schaap.me>
2017-12-08 10:44:35 +0100

    Extract more fields and regex the CVE numbers. Plus pretty print the data for now.

--- ncsc/ncsc.py
+++ ncsc/ncsc.py
@@ -2,7 +2,7 @@
 # Scrape advisories from ncsc.nl
 
 from bs4 import BeautifulSoup
-import requests, gnupg, re
+import requests, gnupg, re, pprint
 
 gpg = None
 
@@ -18,6 +18,18 @@ def parseAdvisoryPage(url):
     advisory['message'] = response.select('.advisoryitem pre')[0].contents[0]
     advisory['verified'] = gpg.verify(advisory['message'])
     advisory['id'] = re.findall(r'Advisory ID\s*:(.+)', advisory['message'])[0].strip()
+    advisory['version'] = re.findall(r'Versie\s*:(.+)', advisory['message'])[0].strip()
+    advisory['probability'] = re.findall(r'Kans\s*:(.+)', advisory['message'])[0].strip()
+    advisory['damage'] = re.findall(r'Schade\s*:(.+)', advisory['message'])[0].strip()
+    advisory['publication_date'] = re.findall(r'Uitgiftedatum\s*:(.+)', advisory['message'])[0].strip()
+    advisory['abstract'] = re.findall(r'Samenvatting(.+)Gevolgen', advisory['message'], re.DOTALL)[0]
+    advisory['effects'] =  re.findall(r'Gevolgen(.+)Beschrijving', advisory['message'], re.DOTALL)[0]
+    advisory['description'] =  re.findall(r'Beschrijving(.+)Mogelijke', advisory['message'], re.DOTALL)[0]
+    advisory['solution'] =  re.findall(r'Mogelijke oplossingen(.+)Vrijwaringsverklaring', advisory['message'], re.DOTALL)[0]
+
+    # Find all CVE IDs in the advisory
+    advisory['CVE'] = set(re.findall(r'(CVE-\d{4}-(?:0\d{3}|[1-9]\d{3,}))', advisory['message']))
+
     return advisory
 
 def main():
@@ -26,7 +38,8 @@ def main():
     url = "https://www.ncsc.nl/dienstverlening/response-op-dreigingen-en-incidenten/beveiligingsadviezen/NCSC-2017-1058+1.00+Kwetsbaarheden+verholpen+in+Apple+iOS.html"
     advisory = parseAdvisoryPage(url)
 
-    print(advisory['id'])
+    pp = pprint.PrettyPrinter(indent=4)
+    pp.pprint(advisory)
 
 if __name__ == '__main__':
     main()
